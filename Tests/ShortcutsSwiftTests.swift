cd1025f9f3475cb668a21162572987063b1463dd
Alexis Aubry <me@alexaubry.fr>
2018-12-24 13:56:01 +0100

    Add test suite

---
+++ Tests/ShortcutsSwiftTests.swift
@@ -0,0 +1,35 @@
+import XCTest
+import ShortcutsSwift
+
+class ShortcutsSwiftTests: XCTestCase {
+
+    /// Tests that the battery level example compiles.
+    func testBatteryExample() {
+        var batteryLevel = Variable.actionOutput()
+        _ = buildShortcut(
+            comment("This Shortcut was generated in Swift.") +
+            getBatteryLevel(&batteryLevel) +
+            ifLessThan(20, ifTrue: (
+                setLowPowerMode(true) +
+                    showResult("Your battery is at \(batteryLevel)%, you might want to charge it.")
+                ), ifFalse: (
+                    showResult("Your battery is at \(batteryLevel)%, you're probably fine for now.")
+            ))
+        )
+    }
+
+    /// Tests that the clap along example compiles.
+    func testClapAlong() {
+        _ = buildShortcut(
+            comment("This Shortcut was generated in Swift.") +
+            ask(question: "WHAT 👏 DO 👏 YOU 👏 WANT 👏 TO 👏 SAY") +
+            changeCase(to: .uppercase) +
+            replaceText("[\\s]", replaceWith: " 👏 ", regularExpression: true) +
+            chooseFromMenu(items: [
+                ("Share", share()),
+                ("Copy to Clipboard", copyToClipboard()),
+            ])
+        )
+    }
+
+}
