63ce68a5488357257fc99e8891b19e55cded25bb
Alexsander Akers <me@a2.io>
2018-12-24 17:21:40 +0100

    Fix indentation in test

--- Tests/ShortcutsSwiftTests.swift
+++ Tests/ShortcutsSwiftTests.swift
@@ -10,10 +10,10 @@ class ShortcutsSwiftTests: XCTestCase {
             comment("This Shortcut was generated in Swift.") +
             getBatteryLevel().savingOutput(to: &batteryLevel) +
             ifLessThan(20, ifTrue: (
-                setLowPowerMode(true) +
-                    showResult("Your battery is at \(batteryLevel)%, you might want to charge it.")
-                ), ifFalse: (
-                    showResult("Your battery is at \(batteryLevel)%, you're probably fine for now.")
+            setLowPowerMode(true) +
+                showResult("Your battery is at \(batteryLevel)%, you might want to charge it.")
+            ), ifFalse: (
+                showResult("Your battery is at \(batteryLevel)%, you're probably fine for now.")
             ))
         )
     }
