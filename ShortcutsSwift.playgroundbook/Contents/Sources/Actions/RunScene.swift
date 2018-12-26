f553de8a8f7555ac99f968ca6df19755be295919
Leonard Thomas <leo.thomas@me.com>
2018-12-26 16:27:10 +0100

    introduce RunScene Action

---
+++ ShortcutsSwift.playgroundbook/Contents/Sources/Actions/RunScene.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+
+/// This methods creates an Action that runs a HomeKit scene
+///
+/// - Parameters:
+///   - home: The name of your home (The default name is "My Home")
+///   - name: The name of the scene you want to run
+public func runScene(for home: String, with name: String) -> ActionContainer {
+    return Action(identifier: "is.workflow.actions.runscene", parameters: ["WFHomeName": home, "WFHomeSceneName": name])
+}
