af7bee1e1436329aa87313f3cbd20e805782e8eb
Alexsander Akers <me@a2.io>
2018-12-26 14:30:20 +0100

    Fix playground book

--- Tests/ShortcutsSwiftTests.swift
+++ Tests/ShortcutsSwiftTests.swift
@@ -9,7 +9,7 @@ class ShortcutsSwiftTests: XCTestCase {
             comment("This Shortcut was generated in Swift.") +
             getBatteryLevel().savingOutput(to: batteryLevel) +
             ifLessThan(20, ifTrue: (
-            setLowPowerMode(true) +
+                setLowPowerMode(true) +
                 showResult("Your battery is at \(batteryLevel)%, you might want to charge it.")
             ), ifFalse: (
                 showResult("Your battery is at \(batteryLevel)%, you're probably fine for now.")
