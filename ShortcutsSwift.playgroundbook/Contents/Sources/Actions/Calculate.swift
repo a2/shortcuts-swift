484087e9215649e81f354c5b52e000fa01feb329
Leonard Thomas <leo.thomas@me.com>
2018-12-26 21:36:00 +0100

    replaced ActionContainer with ActionWithOutput

--- ShortcutsSwift.playgroundbook/Contents/Sources/Actions/Calculate.swift
+++ ShortcutsSwift.playgroundbook/Contents/Sources/Actions/Calculate.swift
@@ -7,7 +7,7 @@ public enum MathOperation: String {
     case divide = "÷"
 }
 
-public func calculate(_ operation: MathOperation, value: Number) -> ActionContainer {
+public func calculate(_ operation: MathOperation, value: Number) -> ActionWithOutput {
     let parameters: PropertyList = [
         "WFMathOperation": operation.rawValue,
         "WFMathOperand": value
@@ -15,7 +15,7 @@ public func calculate(_ operation: MathOperation, value: Number) -> ActionContai
     return Action(identifier: "is.workflow.actions.math", parameters: parameters)
 }
 
-public func calculate(_ operation: MathOperation, with variable: ActionOutputVariable) -> ActionContainer {
+public func calculate(_ operation: MathOperation, with variable: ActionOutputVariable) -> ActionWithOutput {
     let parameters: PropertyList = [
         "WFMathOperation": operation.rawValue,
         "WFMathOperand": variable.propertyList
