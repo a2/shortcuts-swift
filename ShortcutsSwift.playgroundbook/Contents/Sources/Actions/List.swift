370d1d0a7d695b75da25d8a2c3b019bbc7b1e8f2
Leonard Thomas <leo.thomas@me.com>
2018-12-26 20:32:51 +0100

    added support for interpolated list initalisation

--- ShortcutsSwift.playgroundbook/Contents/Sources/Actions/List.swift
+++ ShortcutsSwift.playgroundbook/Contents/Sources/Actions/List.swift
@@ -1,5 +1,22 @@
 import Foundation
 
+struct Item {
+    let type: String
+    let value: PropertyList
+    var propertyList: PropertyList {
+        return ["WFItemType": type,
+                "WFValue" : value
+                ]
+    }
+}
+
 public func list(_ items: [String]) -> ActionWithOutput {
+    let items = items.map { itemString -> Any in
+        let interpolatedText = withInterpolatedText(itemString)
+        if interpolatedText is String {
+            return itemString
+        }
+        return Item(type: "String", value: interpolatedText as! PropertyList).propertyList
+    }
     return Action(identifier: "is.workflow.actions.list", parameters: ["WFItems": items])
 }
